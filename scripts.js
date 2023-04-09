3090b00a34605f6a9ddd11e12698f2a63df8324f
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-04-08 18:00:20 -0700

    fixed typing function

--- scripts.js
+++ scripts.js
@@ -3,12 +3,34 @@ let index = 0;
 
 function type() {
   if (index < typedText.length) {
-    document.querySelector("h1").innerHTML += typedText.charAt(index);
+    document.querySelector("#typedText1").innerHTML += typedText.charAt(index);
     index++;
     setTimeout(type, 45);
   }
+
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
   type();
+});
+
+
+// Back to top button
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+backToTopButton.addEventListener("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, 800);
 });
